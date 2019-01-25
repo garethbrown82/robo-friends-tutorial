9d50aa99003ed90c073ee73eb50a60e7025b7b33
Gareth Brown <garethbrown82@gmail.com>
2019-01-25 14:38:19 +0000

    Updates

---
+++ precache-manifest.c4467d4a463c3dc0e504c54bb0b4f537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends-tutorial/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "54a36a6f612cc3010597",
+    "url": "/robo-friends-tutorial/static/js/runtime~main.54a36a6f.js"
+  },
+  {
+    "revision": "6023bf3fdd910602bf36",
+    "url": "/robo-friends-tutorial/static/js/main.6023bf3f.chunk.js"
+  },
+  {
+    "revision": "470d64a31cfed7f85462",
+    "url": "/robo-friends-tutorial/static/js/1.470d64a3.chunk.js"
+  },
+  {
+    "revision": "6023bf3fdd910602bf36",
+    "url": "/robo-friends-tutorial/static/css/main.e2763f00.chunk.css"
+  },
+  {
+    "revision": "470d64a31cfed7f85462",
+    "url": "/robo-friends-tutorial/static/css/1.a9db8c69.chunk.css"
+  },
+  {
+    "revision": "587dad2f0d725f5f6d39683d809cf44d",
+    "url": "/robo-friends-tutorial/index.html"
+  }
+];
